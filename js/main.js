60bc29132555380f63e6da3ceffdd254a901d02c
Tech-A12 <Tech-A12@Tech-A12s-MacBook-Air.local>
2017-06-25 17:41:59 +0100

    HTML structure of game changed. Buttons included and divs removed to be created dynamically

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(function () {
 
 	// Declare variables;
-
 	var arrayOfImages = ['../p1_online_game/images/angry_farmer.jpg','../p1_online_game/images/chicken.jpg',
 	'../p1_online_game/images/cow.jpg','../p1_online_game/images/dog.jpg','../p1_online_game/images/goat.jpg',
 	'../p1_online_game/images/horse.png','../p1_online_game/images/pig.png', '../p1_online_game/images/sheep.jpg', 
@@ -9,111 +8,120 @@ $(function () {
 	'../p1_online_game/images/cow.jpg','../p1_online_game/images/dog.jpg','../p1_online_game/images/goat.jpg',
 	'../p1_online_game/images/horse.png','../p1_online_game/images/pig.png', '../p1_online_game/images/sheep.jpg'];
 
-	var arrayOfDivs = ['$box1','$box2','$box3','$box4','$box5','$box6','$box7','$box8','$box9','$box10','$box11','$box12',
-	'$box13','$box14','$box15','$box16'];
-
 	var storeChosenImages = [];
-	var storeChosenDivs = [];
-	var boxesFlipped = 0;
+	var storeChosenBoxes = [];
+	var tilesFlipped = 0;
+	var output = 0;
+	var choice1 = 0;
+	var choice2 = 0;
 
 	var $main = $('main');
 	var $div = $('div');
-	var $box1 = $('#box1');
-	var $box2 = $('#box2');
-	var $box3 = $('#box3');
-	var $box4 = $('#box4');
-	var $box5 = $('#box5');
-	var $box6 = $('#box6');
-	var $box7 = $('#box7');
-	var $box8 = $('#box8');
-	var $box9 = $('#box9');
-	var $box10 = $('#box10');
-	var $box11 = $('#box11');
-	var $box12 = $('#box12');
-	var $box13= $('#box13');
-	var $box14 = $('#box14');
-	var $box15= $('#box15');
-	var $box16= $('#box16');
+	var $mainBoard = $('#main-board');
+	
+	// Call functions
+	//createNewBoard();
+
+	//FUNCTIONS
 
 	// Shuffle Array using Fisher-Yates method
-
 	function shuffle(array) {
-  		var currentIndex = arrayOfImages.length, temporaryValue, randomIndex;
+  		var currentIndex = array.length, temporaryValue, randomIndex;
   		while (0 !== currentIndex) {
   			// Pick a remaining element...
     		randomIndex = Math.floor(Math.random() * currentIndex);
     		currentIndex -= 1;
-
 		    // And swap it with the current element.
-		    temporaryValue = arrayOfImages[currentIndex];
-		    arrayOfImages[currentIndex] = arrayOfImages[randomIndex];
-		    arrayOfImages[randomIndex] = temporaryValue;
+		    temporaryValue = array[currentIndex];
+		    array[currentIndex] = array[randomIndex];
+		    array[randomIndex] = temporaryValue;
 		}
-
 		  return array;
 	}
-		arrayOfImages = shuffle(arrayOfImages);
-		//console.log(arrayOfImages);
-
-	function createNewBoard () {
-		boxesFlipped = 0;
+	
+	// Create and load new board
+	function createNewBoard() {
+		tilesFlipped = 0;
 		var output = '';
 		shuffle(arrayOfImages);
-		for (var i = 0; i < arrayOfImages.length; i++) {
-			output = output + "<div id=box" + i + " <img src='" + arrayOfImages[i] + "'/></div>"
+	  for (var i = 0; i < arrayOfImages.length; i++) {
+	        output = "<div id=box" + i + "onclick='flipTile' <img src='" + arrayOfImages[i] + "'/></div>"
+	        //output = "<img src='" + arrayOfImages[i] + "'/>"
+	        $mainBoard.append(output);
+	        }
+	           
+  }
 
-		}
-		$main.html(output);
-	} createNewBoard();
+  	function flipTile(tile,val){
+
+  	}
+
+
+  	$('#div1').click(function(event){
+  		$(this).css('background-color', 'red');
+  	});
+
+  	// $mainBoard.on('click', 'div', function(event) {
+  	// 	if ($(this).attr("class") == "hidden") {
+   //        if (choice1 == undefined) {
+   //            choice1 = $(this);
+   //            choice1.removeClass("hidden").addClass("show");
+   //        }
+   //    }
+
+    	//OpenCard();
+    	//console.log('box2 clicked');
+    	//debugger
+    	//var url = '../p1_online_game/images/angry_farmer.jpg'
+    	//$div.css("background-image", "url(../p1_online_game/images/angry_farmer.jpg) no-repeat");
+    // });
+
+
+	
 
 	//function to flip tile and display images
-	// function displayImage () {
-	// 	if(arrayOfImages < 2 && $div == '') {
+	function displayImage () {
+		if(arrayOfImages.length < 2 && $div.html() == '') {
+			$div.addClass('flipped');
+			//$div.append() = val;
 
-	// 	}
+		}
+	}
 
-	// }
 
 	//create array of image urls
     //var addImage = $("<img src=../p1_online_game/images/sheep.jpg />");
 
-	$main.on('click', 'div',function(event) {
-		console.log('clicked');
-	
+    
+
+	// $main.on('click', 'div', function(event) {
+	// 	var img = $('div img');
+	// 	$(this).html(img).show();
+	// 	//debugger
+	// 	console.log('clicked');
 		
-    }); 
+ //    }); 
 
-    for (var i = 0; i < arrayOfImages.length; i++) {
+    // for (var i = 0; i < arrayOfImages.length; i++) {
     	
-    	var currentImage = arrayOfImages[i];
-    	var addImage = $("<img src='" + currentImage + "'/>");
+    // 	var currentImage = arrayOfImages[i];
+    // 	var addImage = $("<img src='" + currentImage + "'/>");
 
-    	console.log('array' + arrayOfImages);
-    }
+    // 	console.log('array' + arrayOfImages);
+    // }
 
-    for(var j = 0; j < arrayOfDivs.length; j++){
+    // for(var j = 0; j < arrayOfDivs.length; j++){
     		
-    	var currentDiv = arrayOfDivs[j];
-    	console.log('arrayOfDivs');
+    // 	var currentDiv = arrayOfDivs[j];
+    // 	console.log('arrayOfDivs');
     		
-    }
+    // }
 
  //    $div.each(arrayOfImages, function(index, value) {
  //    	$main.append(addImage);
  // })
 
   
-
-    
-
-
-
-	
-		
-
-
-
-
 
 
 });

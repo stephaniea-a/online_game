cef100bebc084cec607a2cbbfba6c383abdddb61
Tech-A12 <Tech-A12@Tech-A12s-MacBook-Air.local>
2017-06-27 10:15:51 +0100

    Win logic added - when match class is equal to 16, win is console logged

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 $(function () {
 
 	// Declare variables;
-	var arrayOfImages = ['../p1_online_game/images/angry_farmer.jpg','../p1_online_game/images/chicken.jpg',
-	'../p1_online_game/images/cow.jpg','../p1_online_game/images/dog.jpg','../p1_online_game/images/goat.jpg',
+	var arrayOfImages = ['../p1_online_game/images/unhappy_farmer.jpg','../p1_online_game/images/chicken.jpg',
+	'../p1_online_game/images/cow.jpg','../p1_online_game/images/dog1.jpg','../p1_online_game/images/goat.jpg',
 	'../p1_online_game/images/horse.png','../p1_online_game/images/pig.png', '../p1_online_game/images/sheep.jpg', 
-	'../p1_online_game/images/angry_farmer.jpg','../p1_online_game/images/chicken.jpg',
-	'../p1_online_game/images/cow.jpg','../p1_online_game/images/dog.jpg','../p1_online_game/images/goat.jpg',
+	'../p1_online_game/images/unhappy_farmer.jpg','../p1_online_game/images/chicken.jpg',
+	'../p1_online_game/images/cow.jpg','../p1_online_game/images/dog1.jpg','../p1_online_game/images/goat.jpg',
 	'../p1_online_game/images/horse.png','../p1_online_game/images/pig.png', '../p1_online_game/images/sheep.jpg'];
 
 	var $gameBoard = $('#gameBoard');
 	var $tiles = $('#tiles');
 	var $counter = $('#counter');
-	var $tilesDiv = $('#tiles div');
 	var $selected = $('.selected');
 
-	var counter = 0;
-	var tileChosen = "";
-	var tileFlipped = "";
-	var match = 0;
+	//var $tilesDiv = $('#tiles div');
+	//var $divImg = $('div img');
 
+	var counter = 0;
 	
 	// Call functions
 	createNewBoard();
@@ -42,13 +40,13 @@ $(function () {
 
 	// Create new boards and place images in random order
 	function createNewBoard() {
+
 		shuffle(arrayOfImages);
 	  	for (var i = 0; i < arrayOfImages.length; i++) {
 	        output = "<div id=box" + i + "><img src='" + arrayOfImages[i] + "'/></div>"
 	        $tiles.append(output);
 	    }
 	        $('div img').hide();
-	        //$('#tiles div img').css('opacity', '0');
   	} 
 
   	// Show image on clicked divs
@@ -56,7 +54,7 @@ $(function () {
   		$(this).addClass('selected');
 		$(this).children().show();
 		//counting clicks and adding to counter
-		$counter.html(function(i, val) { return +val+1 });
+		$counter.html(function(i, val) { return + val + 1 });
 		displayTile();
 
   	});
@@ -64,7 +62,8 @@ $(function () {
   	// Display Tiles
   	function displayTile (){
   		var $selected = $('.selected');
-  		if($selected.length == 2) {
+  		var $match = $('.match');
+  		if($selected.length === 2) {
 
   			var src1 = $selected.eq(1).find('img').attr('src');
   			var src2 = $selected.eq(0).find('img').attr('src');
@@ -73,82 +72,48 @@ $(function () {
   				console.log('its a match');
   				$selected.removeClass('selected');
   				$selected.find('img').addClass('match');
+  				debugger
+
+  				if($('.match').length === 16) {
+
+  					console.log('win');
+  				} else {
+  					console.log('not yet')
+  				}
 
   			} else {
   				console.log('no match');
   				$selected.removeClass('selected');
+  				
   			
 				setTimeout(function() {
-					$selected.find('img').hide();
-				}, 2000);
+					$selected.find('img').fadeOut();
+				}, 1000);
   				
   			}
-  			// console.log('two images have the selected class')
-    		//var lastclicked = "";
+  		
     	}
+
     }
-    
 
- 
+    //when Reset button is clicked
+    $('#reset').click(function(event){
+    	console.log('clicked');
+    	reset();
 
+    });
 
+    function reset() {
+    	shuffle(arrayOfImages);
+    	$('#tiles').html("");
+    	createNewBoard();
 
-
-  		// if ($('#tiles, div').hasClass('selected')){
-  		// 	console.log('has class selected');
-  			//$('#tiles, div').animate({right: -700 +"px"}, 2000);
-  		// }
-  		
-  	
-
-
-
-  		//if ($(this))
-
-  		//debugger
-  		
-            
-        
-
-  		//var id = $(this).attr('src');
-
-  	
-
-  		
+		Counter = 0;
+		$counter.html("" + Counter);
+		return false;
+}
 
 
-  	 
-	
-	
-  	// $mainBoard.on('click', 'div', function(event) {
-  	// 	if ($(this).attr("class") == "hidden") {
-   //        if (choice1 == undefined) {
-   //            choice1 = $(this);
-   //            choice1.removeClass("hidden").addClass("show");
-   //        }
-   //    }
-
-    	//OpenCard();
-    	//console.log('box2 clicked');
-    	//debugger
-    	//var url = '../p1_online_game/images/angry_farmer.jpg'
-    	//$div.css("background-image", "url(../p1_online_game/images/angry_farmer.jpg) no-repeat");
-    // });
-
-	//function to flip tile and display images
-	// function displayImage () {
-	// 	if(arrayOfImages.length < 2 && $div.html() == '') {
-	// 		$div.addClass('flipped');
-	// 		//$div.append() = val;
-
-	// 	}
-	// }
-
- //    $div.each(arrayOfImages, function(index, value) {
- //    	$main.append(addImage);
- // })
-
-  
 
 
 });
